1aff58f227443f05ac5281c6e01eb7188ab2e867
Walter Michel Raja <waltermichelraja@gmail.com>
2026-06-22 20:47:31 +0530

    chore: initial commit

---
+++ backend/src/main/java/com/documentvault/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.documentvault.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/documentvault/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.documentvault.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
