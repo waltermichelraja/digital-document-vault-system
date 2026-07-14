07d49bc70886ea53aa12575355fb718b64568094
Walter Michel Raja <waltermichelraja@gmail.com>
2026-07-14 11:43:41 +0530

    feat(auth): custom user details service

---
+++ backend/src/main/java/com/documentvault/backend/security/userdetails/UserPrincipal.java
@@ -0,0 +1,61 @@
+package com.documentvault.backend.security.userdetails;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.documentvault.backend.entity.User;
+
+public class UserPrincipal implements UserDetails{
+    private final User user;
+
+    public UserPrincipal(User user){
+        this.user=user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of(
+                new SimpleGrantedAuthority(
+                        "ROLE_"+user.getRole().name()
+                )
+        );
+    }
+
+    @Override
+    public String getPassword(){
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername(){
+        return user.getEmail();
+    }
+
+    public User getUser(){
+        return user;
+    }
+
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(){
+        return true;
+    }
+}
